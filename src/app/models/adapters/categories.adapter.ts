5c674080ba5f215e0b4eb1de25f208f31d192490
c4n4r <delphin.hadrien@gmail.com>
2022-12-14 11:59:26 +0100

    alternative

--- src/app/models/adapters/categories.adapter.ts
+++ src/app/models/adapters/categories.adapter.ts
@@ -18,3 +18,30 @@ export const adaptApiToCategory = (input: any): Category[] => {
 
   return categories;
 };
+
+/**
+ * 
+ * Alternative un peu plus old school : 
+ * 
+ *export function adaptApiToCategory(input: any): Category[] {
+  const categories: Category[] = [];
+
+  //adapt
+  input.data.map((element: any) => {
+    const category: Category = {
+      id: element.id,
+      name: element.attributes.name,
+      image: {
+        thumbnail: `http://localhost:1337${element.attributes.image.data.attributes.formats.thumbnail.url}`,
+        medium: `http://localhost:1337${element.attributes.image.data.attributes.formats.medium.url}`,
+      },
+    };
+    categories.push(category);
+  });
+
+  return categories;
+}
+ * 
+ * 
+ * 
+ */
