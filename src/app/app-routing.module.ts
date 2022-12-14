c9fa77a2d43ff8c28b5b0c0e51f8d46b75d6b87d
c4n4r <delphin.hadrien@gmail.com>
2022-12-14 13:47:53 +0100

    init branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'home',
     component: HomePageComponent,
+  },
+  {
+    path: 'skills',
+    component: SkillsPageComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CategoriesService } from './services/categories.service';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomePageComponent],
+  declarations: [AppComponent, HeaderComponent, HomePageComponent, SkillsPageComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [CategoriesService],
   bootstrap: [AppComponent],
